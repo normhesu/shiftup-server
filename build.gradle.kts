3cab0d6864e1ab137487486bd21060a897f1fc3d
normhesu <normhesu@gmail.com>
2022-07-21 12:20:37 +0900

    chore: Gradle Shadowを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     application
     kotlin("jvm") version "1.7.10"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "app.vercel.shiftup"
