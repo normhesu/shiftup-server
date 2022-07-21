381c4d18a70afeb4d2794f7a1bb5142a91890d68
normhesu <normhesu@gmail.com>
2022-07-21 11:24:06 +0900

    chore: サーバーの作成方法をEngineMain﻿に変更

--- src/main/kotlin/app/vercel/shiftup/Application.kt
+++ src/main/kotlin/app/vercel/shiftup/Application.kt
@@ -1,15 +1,16 @@
 package app.vercel.shiftup
 
 import app.vercel.shiftup.plugins.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.application.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureSerialization()
-        configureMonitoring()
-        configureHTTP()
-        configureSecurity()
-        configureRouting()
-    }.start(wait = true)
+@Suppress("RemoveRedundantQualifierName")
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
 }
