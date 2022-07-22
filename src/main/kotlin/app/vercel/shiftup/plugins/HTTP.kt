0f214c7ebd83245ba6788b0bf67aedb6a2645ad9
normhesu <normhesu@gmail.com>
2022-07-22 16:28:51 +0900

    chore: CORSの設定を変更

--- src/main/kotlin/app/vercel/shiftup/plugins/HTTP.kt
+++ src/main/kotlin/app/vercel/shiftup/plugins/HTTP.kt
@@ -6,19 +6,26 @@ import io.ktor.server.plugins.cors.routing.*
 import org.mpierce.ktor.csrf.CsrfProtection
 import org.mpierce.ktor.csrf.OriginMatchesKnownHost
 
+private const val SCHEME = "https"
+private const val HOST = "shiftup.vercel.app"
+
 fun Application.configureSecurity() {
+    val productMode = environment.developmentMode.not()
     install(CORS) {
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        if (productMode) {
+            allowHost(host = HOST, schemes = listOf(SCHEME))
+        } else {
+            anyHost()
+        }
     }
 
-    if (environment.developmentMode.not()) install(CsrfProtection) {
+    if (productMode) install(CsrfProtection) {
         applyToAllRoutes()
-        validate(OriginMatchesKnownHost("https", "shiftup.vercel.app"))
+        validate(OriginMatchesKnownHost(SCHEME, HOST))
     }
 }
