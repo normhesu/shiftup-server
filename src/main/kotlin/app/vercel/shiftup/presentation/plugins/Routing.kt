f3fd79ede8b9276b1c471f9306d36d8f92ae086e
normhesu <normhesu@gmail.com>
2022-07-31 23:20:46 +0900

    chore: StatusPagesの設定を追加

--- src/main/kotlin/app/vercel/shiftup/presentation/plugins/Routing.kt
+++ src/main/kotlin/app/vercel/shiftup/presentation/plugins/Routing.kt
@@ -1,15 +1,39 @@
 package app.vercel.shiftup.presentation.plugins
 
 import app.vercel.shiftup.presentation.routes.routes
+import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.resources.*
+import io.ktor.server.response.*
 
 fun Application.configureRouting() {
     install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            when (cause) {
+                is IllegalArgumentException -> call.respondText(
+                    status = HttpStatusCode.UnprocessableEntity,
+                    cause = cause,
+                )
+                else -> call.respondText(
+                    status = HttpStatusCode.InternalServerError,
+                    cause = cause.takeIf { call.application.developmentMode }
+                )
+            }
+            throw cause
+        }
+
+        val respondTextStatusCodes = HttpStatusCode.allStatusCodes
+            .filter { it.isSuccess().not() }
+            .toTypedArray()
+        @Suppress("SpreadOperator")
+        status(*respondTextStatusCodes) { call, status ->
+            call.respondText(status)
+        }
     }
     install(AutoHeadResponse)
     install(Resources)
@@ -17,4 +41,20 @@ fun Application.configureRouting() {
         json()
     }
     routes()
+}
+
+private suspend fun ApplicationCall.respondText(
+    status: HttpStatusCode,
+    cause: Throwable? = null,
+    contentType: ContentType? = null,
+    configure: OutgoingContent.() -> Unit = {}
+) {
+    val statusText = "${status.value} ${status.description}"
+    val causeText = cause?.let { "\n\n$it" }.orEmpty()
+    this.respondText(
+        text = statusText + causeText,
+        status = status,
+        contentType = contentType,
+        configure = configure,
+    )
 }
