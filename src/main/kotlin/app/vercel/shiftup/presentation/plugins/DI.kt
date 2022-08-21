1eac1726e360fceb313a0743a4f285a51cf25a06
normhesu <normhesu@gmail.com>
2022-08-22 06:04:30 +0900

    refactor: DefaultModuleGenerationを削除

--- src/main/kotlin/app/vercel/shiftup/presentation/plugins/DI.kt
+++ src/main/kotlin/app/vercel/shiftup/presentation/plugins/DI.kt
@@ -2,7 +2,6 @@ package app.vercel.shiftup.presentation.plugins
 
 import app.vercel.shiftup.kmongoModule
 import io.ktor.server.application.*
-import org.koin.core.annotation.Single
 import org.koin.ksp.generated.defaultModule
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
@@ -13,7 +12,3 @@ fun Application.configureDI() {
         modules(defaultModule, kmongoModule)
     }
 }
-
-@Single
-@Deprecated("KoinのdefaultModule生成のみに使用します")
-class DefaultModuleGeneration
