839885761a1edd1299b8e0af8117fe72256704f3
normhesu <normhesu@gmail.com>
2022-08-21 16:17:05 +0900

    feat: Kmongo用の拡張関数を追加

---
+++ src/main/kotlin/app/vercel/shiftup/features/core/infra/KmongoResultExtensions.kt
@@ -0,0 +1,27 @@
+package app.vercel.shiftup.features.core.infra
+
+import com.mongodb.client.result.DeleteResult
+import com.mongodb.client.result.InsertManyResult
+import com.mongodb.client.result.InsertOneResult
+import com.mongodb.client.result.UpdateResult
+import io.ktor.utils.io.errors.*
+
+fun InsertOneResult.orThrow(): InsertOneResult {
+    if (!wasAcknowledged()) throw IOException()
+    return this
+}
+
+fun InsertManyResult.orThrow(): InsertManyResult {
+    if (!wasAcknowledged()) throw IOException()
+    return this
+}
+
+fun UpdateResult.orThrow(): UpdateResult {
+    if (!wasAcknowledged()) throw IOException()
+    return this
+}
+
+fun DeleteResult.orThrow(): DeleteResult {
+    if (!wasAcknowledged()) throw IOException()
+    return this
+}
