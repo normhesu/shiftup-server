6fb8788dc98fb00b9efbfb9194ba6076970412d9
normhesu <normhesu@gmail.com>
2022-08-24 00:03:00 +0900

    test:: ドメインモデル層のテストにクラス名のグループを追加

--- src/test/kotlin/app/vercel/shiftup/features/user/domain/model/value/EntranceYearTest.kt
+++ src/test/kotlin/app/vercel/shiftup/features/user/domain/model/value/EntranceYearTest.kt
@@ -10,101 +10,103 @@ import java.time.ZoneId
 import java.time.ZonedDateTime
 
 class EntranceYearTest : FreeSpec({
-    "getSchoolYear" - {
-        mockkStatic(ZonedDateTime::class)
+    "EntranceYear" - {
+        "getSchoolYear" - {
+            mockkStatic(ZonedDateTime::class)
 
-        data class Params(
-            val currentYear: Int,
-            val currentMonth: Int,
-            val entranceYearValue: Int,
-            val tenureValue: Int,
-            val expectedSchoolYearValue: Int?,
-        )
+            data class Params(
+                val currentYear: Int,
+                val currentMonth: Int,
+                val entranceYearValue: Int,
+                val tenureValue: Int,
+                val expectedSchoolYearValue: Int?,
+            )
 
-        fun invokeTest(vararg params: Params) {
-            val tokyoZoneId = ZoneId.of("Asia/Tokyo")
-            params.forAll {
-                every {
-                    ZonedDateTime.now(tokyoZoneId)
-                } returns ZonedDateTime.of(
-                    LocalDateTime.of(
-                        it.currentYear, it.currentMonth,
-                        1, 0, 0,
-                    ),
-                    tokyoZoneId,
-                )
+            fun invokeTest(vararg params: Params) {
+                val tokyoZoneId = ZoneId.of("Asia/Tokyo")
+                params.forAll {
+                    every {
+                        ZonedDateTime.now(tokyoZoneId)
+                    } returns ZonedDateTime.of(
+                        LocalDateTime.of(
+                            it.currentYear, it.currentMonth,
+                            1, 0, 0,
+                        ),
+                        tokyoZoneId,
+                    )
 
-                EntranceYear(it.entranceYearValue).getSchoolYear(
-                    tenure = Tenure(it.tenureValue),
-                ) shouldBe it
-                    .expectedSchoolYearValue
-                    ?.let(::SchoolYear)
+                    EntranceYear(it.entranceYearValue).getSchoolYear(
+                        tenure = Tenure(it.tenureValue),
+                    ) shouldBe it
+                        .expectedSchoolYearValue
+                        ?.let(::SchoolYear)
+                }
             }
-        }
 
-        "在学中の場合は学年を返す" {
-            invokeTest(
-                Params(
-                    currentYear = 2022, currentMonth = 3,
-                    entranceYearValue = 2021, tenureValue = 2,
-                    expectedSchoolYearValue = 1,
-                ),
-                Params(
-                    currentYear = 2022, currentMonth = 4,
-                    entranceYearValue = 2021, tenureValue = 2,
-                    expectedSchoolYearValue = 2,
-                ),
-                Params(
-                    currentYear = 2023, currentMonth = 4,
-                    entranceYearValue = 2021, tenureValue = 4,
-                    expectedSchoolYearValue = 3,
-                ),
-                Params(
-                    currentYear = 2024, currentMonth = 4,
-                    entranceYearValue = 2021, tenureValue = 4,
-                    expectedSchoolYearValue = 4,
-                ),
-            )
-        }
+            "在学中の場合は学年を返す" {
+                invokeTest(
+                    Params(
+                        currentYear = 2022, currentMonth = 3,
+                        entranceYearValue = 2021, tenureValue = 2,
+                        expectedSchoolYearValue = 1,
+                    ),
+                    Params(
+                        currentYear = 2022, currentMonth = 4,
+                        entranceYearValue = 2021, tenureValue = 2,
+                        expectedSchoolYearValue = 2,
+                    ),
+                    Params(
+                        currentYear = 2023, currentMonth = 4,
+                        entranceYearValue = 2021, tenureValue = 4,
+                        expectedSchoolYearValue = 3,
+                    ),
+                    Params(
+                        currentYear = 2024, currentMonth = 4,
+                        entranceYearValue = 2021, tenureValue = 4,
+                        expectedSchoolYearValue = 4,
+                    ),
+                )
+            }
 
-        "入学前の場合はnullを返す" {
-            invokeTest(
-                Params(
-                    currentYear = 2021, currentMonth = 3,
-                    entranceYearValue = 2021, tenureValue = 2,
-                    expectedSchoolYearValue = null,
-                ),
-                Params(
-                    currentYear = 2021, currentMonth = 3,
-                    entranceYearValue = 2021, tenureValue = 3,
-                    expectedSchoolYearValue = null,
-                ),
-                Params(
-                    currentYear = 2021, currentMonth = 4,
-                    entranceYearValue = 2022, tenureValue = 4,
-                    expectedSchoolYearValue = null,
-                ),
-            )
-        }
+            "入学前の場合はnullを返す" {
+                invokeTest(
+                    Params(
+                        currentYear = 2021, currentMonth = 3,
+                        entranceYearValue = 2021, tenureValue = 2,
+                        expectedSchoolYearValue = null,
+                    ),
+                    Params(
+                        currentYear = 2021, currentMonth = 3,
+                        entranceYearValue = 2021, tenureValue = 3,
+                        expectedSchoolYearValue = null,
+                    ),
+                    Params(
+                        currentYear = 2021, currentMonth = 4,
+                        entranceYearValue = 2022, tenureValue = 4,
+                        expectedSchoolYearValue = null,
+                    ),
+                )
+            }
 
-        "卒業後の場合はnullを返す" {
-            invokeTest(
-                Params(
-                    currentYear = 2023, currentMonth = 4,
-                    entranceYearValue = 2021, tenureValue = 2,
-                    expectedSchoolYearValue = null,
-                ),
-                Params(
-                    currentYear = 2024, currentMonth = 4,
-                    entranceYearValue = 2021, tenureValue = 3,
-                    expectedSchoolYearValue = null,
-                ),
-                Params(
-                    currentYear = 2025, currentMonth = 4,
-                    entranceYearValue = 2021, tenureValue = 4,
-                    expectedSchoolYearValue = null,
-                ),
-            )
+            "卒業後の場合はnullを返す" {
+                invokeTest(
+                    Params(
+                        currentYear = 2023, currentMonth = 4,
+                        entranceYearValue = 2021, tenureValue = 2,
+                        expectedSchoolYearValue = null,
+                    ),
+                    Params(
+                        currentYear = 2024, currentMonth = 4,
+                        entranceYearValue = 2021, tenureValue = 3,
+                        expectedSchoolYearValue = null,
+                    ),
+                    Params(
+                        currentYear = 2025, currentMonth = 4,
+                        entranceYearValue = 2021, tenureValue = 4,
+                        expectedSchoolYearValue = null,
+                    ),
+                )
+            }
         }
     }
 })
--- src/test/kotlin/app/vercel/shiftup/features/user/domain/model/value/StudentNumberTest.kt
+++ src/test/kotlin/app/vercel/shiftup/features/user/domain/model/value/StudentNumberTest.kt
@@ -6,16 +6,18 @@ import io.kotest.property.arbitrary.int
 import io.kotest.property.forAll
 
 class StudentNumberTest : FreeSpec({
-    "学年を取得" {
-        forAll(Arb.int(0..999)) { entranceYearValue ->
-            val studentNumber = StudentNumber(
-                "G${"$entranceYearValue".padStart(3, '0')}C0000",
-            )
-            val entranceYear = EntranceYear(
-                entranceYearValue + 2000,
-            )
-            val tenure = Department.C2.tenure
-            studentNumber.getSchoolYear(tenure) == entranceYear.getSchoolYear(tenure)
+    "StudentNumber" - {
+        "学年を取得" {
+            forAll(Arb.int(0..999)) { entranceYearValue ->
+                val studentNumber = StudentNumber(
+                    "G${"$entranceYearValue".padStart(3, '0')}C0000",
+                )
+                val entranceYear = EntranceYear(
+                    entranceYearValue + 2000,
+                )
+                val tenure = Department.C2.tenure
+                studentNumber.getSchoolYear(tenure) == entranceYear.getSchoolYear(tenure)
+            }
         }
     }
 })
