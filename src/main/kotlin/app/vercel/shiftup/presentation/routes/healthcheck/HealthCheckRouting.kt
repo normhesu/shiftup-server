e680588a73b68dc9ca34195fc095d2e906c271e3
normhesu <normhesu@gmail.com>
2022-11-15 15:07:58 +0900

    feat: 動作確認用のルートを追加

--- src/main/kotlin/app/vercel/shiftup/presentation/routes/healthcheck/HealthCheckRouting.kt
+++ src/main/kotlin/app/vercel/shiftup/presentation/routes/healthcheck/HealthCheckRouting.kt
@@ -1,16 +1,66 @@
 package app.vercel.shiftup.presentation.routes.healthcheck
 
+import app.vercel.shiftup.features.user.account.application.GetUserUseCase
+import app.vercel.shiftup.features.user.account.domain.model.UserId
+import io.ktor.resources.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.resources.*
+import io.ktor.server.resources.patch
+import io.ktor.server.resources.post
+import io.ktor.server.resources.put
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import kotlinx.serialization.Serializable
+import org.koin.ktor.ext.inject
 import org.mpierce.ktor.csrf.noCsrfProtection
 
 fun Application.healthCheckRouting() {
     routing {
         noCsrfProtection {
-            get("/health-check") {
+            get<HealthCheck> {
                 call.respondText("OK")
             }
+            get<HealthCheck.DB> {
+                val getUserUseCase: GetUserUseCase by application.inject()
+                getUserUseCase(UserId(""))
+                call.respondText("DB OK")
+            }
+            authenticate {
+                get<HealthCheck.Authentication> {
+                    call.respondText("Authentication OK")
+                }
+            }
+        }
+        post<HealthCheck.CSRF> {
+            call.respondText("CSRF OK")
+        }
+        patch<HealthCheck.CSRF> {
+            call.respondText("CSRF OK")
+        }
+        put<HealthCheck.CSRF> {
+            call.respondText("CSRF OK")
+        }
+        delete<HealthCheck.CSRF> {
+            call.respondText("CSRF OK")
         }
     }
+}
+
+@Suppress("unused")
+@Serializable
+@Resource("/health-check")
+object HealthCheck {
+    @Serializable
+    @Resource("db")
+    class DB(val parent: HealthCheck = HealthCheck)
+
+    @Serializable
+    @Resource("csrf")
+    class CSRF(val parent: HealthCheck = HealthCheck)
+
+    @Serializable
+    @Resource("authentication")
+    class Authentication(val parent: HealthCheck = HealthCheck)
 }
