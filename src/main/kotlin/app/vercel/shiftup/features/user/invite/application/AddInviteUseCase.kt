3781f4cf479c27c8110a553d542e094a811b5bef
normhesu <normhesu@gmail.com>
2022-12-23 13:43:47 +0900

    refactor: AddInviteUseCaseで招待済みかどうかを確認する処理を変更

--- src/main/kotlin/app/vercel/shiftup/features/user/invite/application/AddInviteUseCase.kt
+++ src/main/kotlin/app/vercel/shiftup/features/user/invite/application/AddInviteUseCase.kt
@@ -4,21 +4,22 @@ import app.vercel.shiftup.features.user.invite.domain.model.Invite
 import app.vercel.shiftup.features.user.invite.infra.InviteRepository
 import com.github.michaelbull.result.coroutines.runSuspendCatching
 import com.github.michaelbull.result.mapError
+import com.mongodb.MongoException
 import org.koin.core.annotation.Single
 
 @Single
 class AddInviteUseCase(
     private val inviteRepository: InviteRepository,
 ) {
+    companion object {
+        private const val DUPLICATE_KEY_CODE = 11000
+    }
+
     suspend operator fun invoke(invite: Invite) = runSuspendCatching<Unit> {
-        val invited = inviteRepository.findByStudentNumber(
-            invite.studentNumber,
-        ) != null
-        if (invited) throw InvitedException()
         inviteRepository.add(invite)
     }.mapError {
-        if (it !is InvitedException) throw it
-        it
+        val invited = it is MongoException && it.code == DUPLICATE_KEY_CODE
+        if (invited) InvitedException() else throw it
     }
 }
 
