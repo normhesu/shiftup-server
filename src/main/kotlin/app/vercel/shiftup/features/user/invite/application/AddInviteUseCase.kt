1a667117e18ffa565da6e3a326d54affb415037f
normhesu <normhesu@gmail.com>
2023-01-24 10:30:57 +0900

    fix: firstManagerの招待追加時にpositionがManagerかどうか確認するように変更

--- src/main/kotlin/app/vercel/shiftup/features/user/invite/application/AddInviteUseCase.kt
+++ src/main/kotlin/app/vercel/shiftup/features/user/invite/application/AddInviteUseCase.kt
@@ -1,28 +1,40 @@
 package app.vercel.shiftup.features.user.invite.application
 
 import app.vercel.shiftup.features.user.invite.domain.model.Invite
+import app.vercel.shiftup.features.user.invite.domain.model.value.FirstManager
+import app.vercel.shiftup.features.user.invite.domain.model.value.Position
 import app.vercel.shiftup.features.user.invite.infra.InviteRepository
-import com.github.michaelbull.result.coroutines.runSuspendCatching
+import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.mapError
+import com.github.michaelbull.result.runCatching
+import com.github.michaelbull.result.throwIf
 import com.mongodb.MongoException
 import org.koin.core.annotation.Single
 
 @Single
 class AddInviteUseCase(
     private val inviteRepository: InviteRepository,
+    private val firstManager: FirstManager,
 ) {
     companion object {
         private const val DUPLICATE_KEY_CODE = 11000
     }
 
-    suspend operator fun invoke(invite: Invite) = runSuspendCatching<Unit> {
-        inviteRepository.add(invite)
-    }.mapError {
-        val invited = it is MongoException && it.code == DUPLICATE_KEY_CODE
-        if (invited) AddInviteUseCaseException.Invited else throw it
+    suspend operator fun invoke(invite: Invite): Result<Unit, AddInviteUseCaseException> {
+        if (invite.studentNumber == firstManager.studentNumber) {
+            require(invite.position == Position.Manager)
+        }
+
+        return runCatching {
+            inviteRepository.add(invite)
+        }.throwIf {
+            (it is MongoException && it.code == DUPLICATE_KEY_CODE).not()
+        }.mapError {
+            AddInviteUseCaseException.Invited(it)
+        }
     }
 }
 
-sealed interface AddInviteUseCaseException {
-    object Invited : Exception(), AddInviteUseCaseException
+sealed class AddInviteUseCaseException(cause: Throwable) : Exception(cause) {
+    class Invited(cause: Throwable) : AddInviteUseCaseException(cause)
 }
--- src/test/kotlin/app/vercel/shiftup/features/user/invite/application/AddInviteUseCaseTest.kt
+++ src/test/kotlin/app/vercel/shiftup/features/user/invite/application/AddInviteUseCaseTest.kt
@@ -1,11 +1,18 @@
 package app.vercel.shiftup.features.user.invite.application
 
+import app.vercel.shiftup.features.user.domain.model.value.Email
+import app.vercel.shiftup.features.user.domain.model.value.NeecDepartment
+import app.vercel.shiftup.features.user.domain.model.value.SchoolProfile
 import app.vercel.shiftup.features.user.domain.model.value.StudentNumber
 import app.vercel.shiftup.features.user.invite.domain.model.Invite
+import app.vercel.shiftup.features.user.invite.domain.model.value.FirstManager
 import app.vercel.shiftup.features.user.invite.infra.InviteRepository
 import com.github.michaelbull.result.Err
 import com.mongodb.MongoException
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -14,42 +21,86 @@ import io.mockk.mockk
 class AddInviteUseCaseTest : FreeSpec({
     "AddInviteUseCase" - {
         val repository: InviteRepository = mockk(relaxUnitFun = true)
-        val useCase = AddInviteUseCase(
-            inviteRepository = repository,
-        )
-
         val studentNumber = StudentNumber("G000C0000")
         val invite = Invite(
             studentNumber = studentNumber,
             mockk(relaxed = true), mockk(relaxed = true),
         )
 
-        "ユーザーがまだ招待されていない場合は追加する" {
-            coEvery {
-                repository.findByStudentNumber(studentNumber)
-            } returns null
+        "正常系" - {
+            "ユーザーがまだ招待されていない場合は追加する" {
+                val useCase = AddInviteUseCase(
+                    inviteRepository = repository,
+                    firstManager = FirstManager(
+                        schoolProfile = SchoolProfile(
+                            department = NeecDepartment.C2,
+                            email = Email("g000c9999@g.neec.ac.jp").also {
+                                it.studentNumber shouldNotBe studentNumber
+                            }
+                        )
+                    ),
+                )
+                coEvery {
+                    repository.findByStudentNumber(studentNumber)
+                } returns null
 
-            useCase(invite)
-            coVerify(exactly = 1) {
-                repository.add(invite)
+                useCase(invite)
+                coVerify(exactly = 1) {
+                    repository.add(invite)
+                }
             }
         }
 
-        "ユーザーが既に招待されている場合は、追加せずにErr<AddInviteUseCaseException.Invited>を返す" {
-            val duplicateKeyCode = 11000
-            val mockMongoException: MongoException = mockk(relaxed = true)
+        "異常系" - {
+            "ユーザーが既に招待されている場合は、追加せずにErr<AddInviteUseCaseException.Invited>を返す" {
+                val useCase = AddInviteUseCase(
+                    inviteRepository = repository,
+                    firstManager = FirstManager(
+                        schoolProfile = SchoolProfile(
+                            department = NeecDepartment.C2,
+                            email = Email("g000c9999@g.neec.ac.jp").also {
+                                it.studentNumber shouldNotBe studentNumber
+                            }
+                        )
+                    ),
+                )
 
-            coEvery {
-                mockMongoException.code
-            } answers {
-                duplicateKeyCode
+                val duplicateKeyCode = 11000
+                val mockMongoException: MongoException = mockk(relaxed = true)
+
+                coEvery {
+                    mockMongoException.code
+                } answers {
+                    duplicateKeyCode
+                }
+
+                coEvery {
+                    repository.add(invite)
+                } throws mockMongoException
+
+                useCase(invite).shouldBeInstanceOf<Err<AddInviteUseCaseException.Invited>>()
             }
 
-            coEvery {
-                repository.add(invite)
-            } throws mockMongoException
+            "ユーザーがfirstManagerとして設定されている場合、positionがManager出なければIllegalArgumentExceptionをthrowする" {
+                val useCase = AddInviteUseCase(
+                    inviteRepository = repository,
+                    firstManager = FirstManager(
+                        schoolProfile = SchoolProfile(
+                            department = NeecDepartment.C2,
+                            email = Email("g000c0000@g.neec.ac.jp").also {
+                                it.studentNumber shouldBe studentNumber
+                            }
+                        )
+                    ),
+                )
+                coEvery {
+                    repository.findByStudentNumber(studentNumber)
+                } returns null
 
-            useCase(invite).shouldBeInstanceOf<Err<AddInviteUseCaseException.Invited>>()
+                shouldThrow<IllegalArgumentException> {
+                    useCase(invite)
+                }
+            }
         }
     }
 })
